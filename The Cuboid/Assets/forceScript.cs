e343665952c09e84441f1ae6f80befc22ffc5f12
STUDENT Jason O Connor <Jason.OConnnor@students.ittralee.ie>
2022-10-13 10:51:53 +0100

    added a semi finished shoot method. Signed-off-by: STUDENT Jason O Connor <Jason.OConnnor@students.ittralee.ie>

--- The Cuboid/Assets/forceScript.cs
+++ The Cuboid/Assets/forceScript.cs
@@ -10,32 +10,34 @@ public class forceScript : MonoBehaviour
     void Start()
     {
         ourRigidBody = GetComponent<Rigidbody>();
+        if (Input.GetKeyDown(KeyCode.B))
+            ourRigidBody.AddExplosionForce(1000, transform.position + transform.forward, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            ourRigidBody.AddExplosionForce(1000,transform.position + Vector3.down, 2) ;
-        }
+       // if (Input.GetKeyDown(KeyCode.LeftArrow))
+        
+         //   ourRigidBody.AddExplosionForce(1000,transform.position + Vector3.down, 2) ;
+        
     }
     private void OnCollisionEnter(Collision collision)
     {
-       planeBehavior objectHitplanebehavior = collision.gameObject.GetComponent< planeBehavior > ();
-        if (objectHitplanebehavior)
-        {
-            print("found planeBehavior in object hit");
-            objectHitplanebehavior.takeDamage(20);
+       //planeBehavior objectHitplanebehavior = collision.gameObject.GetComponent< planeBehavior > ();
+       // if (objectHitplanebehavior)
+      //  {
+        //    print("found planeBehavior in object hit");
+        //    objectHitplanebehavior.takeDamage(20);
 
-            int ObjectsMaxHealth = objectHitplanebehavior.whatsYourMaxHealth();
-            if (ObjectsMaxHealth > 100)
-                objectHitplanebehavior .takeDamage(400);
+          //  int ObjectsMaxHealth = objectHitplanebehavior.whatsYourMaxHealth();
+          //  if (ObjectsMaxHealth > 100)
+          //      objectHitplanebehavior .takeDamage(400);
         }
-        else
-        {
-            print("didnt find planeBehavior in object hit");
-        }
+       // else
+        
+          //  print("didnt find planeBehavior in object hit");
+        
         // print("Ouch!");
-    }
+    
 }
