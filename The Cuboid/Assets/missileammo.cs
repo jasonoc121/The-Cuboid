fb6fae0984f40082dfab4d5e12e9ae731784d489
jasonoc121 <jasonoconnor9@gmail.com>
2022-10-26 23:04:37 +0100

    added the missile script to tank, may have t change missile shape as i cant seem to get rotation right. Signed-off-by: jasonoc121 <jasonoconnor9@gmail.com>

--- The Cuboid/Assets/missileammo.cs
+++ The Cuboid/Assets/missileammo.cs
@@ -7,27 +7,35 @@ public class missileammo : MonoBehaviour
     Rigidbody missileBody;
     public GameObject destroyObject;
     ParticleSystem particle;
+    public Transform missileTemp;
     // Start is called before the first frame update
     void Start()
     {
         missileBody = GetComponent<Rigidbody>();
-        if (Input.GetKeyDown(KeyCode.Space))
-            missileBody.AddExplosionForce(1000, transform.position + transform.forward, 1);
+        //if (Input.GetKeyDown(KeyCode.Space))
+          //  missileBody.AddExplosionForce(1000, transform.position + transform.forward, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Transform newProjectile = Instantiate(missileTemp, transform.position + transform.forward,
+                   transform.rotation);
+            Rigidbody projectileRB = newProjectile.GetComponent<Rigidbody>();
+            projectileRB.useGravity = false;
+            projectileRB.AddExplosionForce(100, newProjectile.transform.position - newProjectile.transform.forward, 3);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        missile objecthitdetected = collision.gameObject.GetComponent<missile>();
-        if (objecthitdetected)
+       // missile objecthitdetected = collision.gameObject.GetComponent<missile>();
+       // if (objecthitdetected)
         {
-            particle.Play();
-            Destroy(destroyObject, particle.main.duration);
+       //     particle.Play();
+       //     Destroy(destroyObject, particle.main.duration);
 
         }
     }
