afe435963a8d56655246e00e1027832e32e6d8ac
STUDENT Jason O Connor <Jason.OConnnor@students.ittralee.ie>
2022-10-27 14:24:58 +0100

    SCRAPPED THE NEW SCENE, CHANGED MISSILE SHAPE AND GOT THE PARTICLE EMITTER TO WORK AT A SOMEWHAT SATISFACTORY LEVEL. Signed-off-by: STUDENT Jason O Connor <Jason.OConnnor@students.ittralee.ie>

--- The Cuboid/Assets/forceScript.cs
+++ The Cuboid/Assets/forceScript.cs
@@ -5,39 +5,50 @@ using UnityEngine;
 public class forceScript : MonoBehaviour
 {
     Rigidbody ourRigidBody;
+    public GameObject destroyObject;
+    ParticleSystem particle;
 
     // Start is called before the first frame update
     void Start()
     {
+        particle = GetComponent<ParticleSystem>();
         ourRigidBody = GetComponent<Rigidbody>();
-        if (Input.GetKeyDown(KeyCode.B))
-            ourRigidBody.AddExplosionForce(1000, transform.position + transform.forward, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-       // if (Input.GetKeyDown(KeyCode.LeftArrow))
-        
-         //   ourRigidBody.AddExplosionForce(1000,transform.position + Vector3.down, 2) ;
-        
+
+
     }
     private void OnCollisionEnter(Collision collision)
     {
-       //planeBehavior objectHitplanebehavior = collision.gameObject.GetComponent< planeBehavior > ();
-       // if (objectHitplanebehavior)
-      //  {
+        
+            missile objecthitdetected = collision.gameObject.GetComponent<missile>();
+            if (objecthitdetected)
+            {
+                particle.Play();
+                Destroy(destroyObject, particle.main.duration);
+            }
+            else
+        {
+
+
+        }
+        //planeBehavior objectHitplanebehavior = collision.gameObject.GetComponent< planeBehavior > ();
+        // if (objectHitplanebehavior)
+
         //    print("found planeBehavior in object hit");
         //    objectHitplanebehavior.takeDamage(20);
 
-          //  int ObjectsMaxHealth = objectHitplanebehavior.whatsYourMaxHealth();
-          //  if (ObjectsMaxHealth > 100)
-          //      objectHitplanebehavior .takeDamage(400);
-        }
-       // else
-        
-          //  print("didnt find planeBehavior in object hit");
-        
+        //  int ObjectsMaxHealth = objectHitplanebehavior.whatsYourMaxHealth();
+        //  if (ObjectsMaxHealth > 100)
+        //      objectHitplanebehavior .takeDamage(400);
+
+        // else
+
+        //  print("didnt find planeBehavior in object hit");
+
         // print("Ouch!");
-    
+    }
 }
